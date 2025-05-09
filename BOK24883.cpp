73f07d9358233ee5bd6c2534f168c0336fff504c
Khyojae <khjae201@gmail.com>
2025-05-09 20:59:50 +0900

    24883

---
+++ BOK24883.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string str1;
+    cin >> str1;
+    if(str1=="N"){
+
+cout << "Naver D2";
+    }
+
+ else if(str1=="n"){
+
+  cout << "Naver D2";      
+    }
+
+else{
+    cout << "Naver Whale";
+}
+    
+}
