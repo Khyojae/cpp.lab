203fa3cb9f34c30b63b6b54ed4d25996ea213219
Khyojae <khjae201@gmail.com>
2025-05-16 20:37:14 +0900

    10039

---
+++ BOK2752.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a[5];
+int b=0;
+
+for(int i=0; i<=4; i++){
+cin >> a[i];
+if(a[i]<40){
+    a[i]==40;
+}
+}
+
+for(int i=0; i<=4; i++){
+    b= 0+ a[i];
+}
+cout << b/5;
+
+}
