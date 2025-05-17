5e01051476a33968dd2626baf0c4523082c73a42
Khyojae <khjae201@gmail.com>
2025-05-18 02:43:14 +0900

    최댓값 만들기

---
+++ BOK9506.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+int arr[3];
+int brr[10];
+int n;
+
+for(int i=0; i<=2; i++){
+arr[i]=n;
+}
+
+
+for(int i=0; i<=2; i++){
+   for(int p=0; p<=arr[i]; p++){
+    if(arr[i]%p ==0){
+        brr[i]=p;
+    }
+
+   }
+   for(int k=0; k<=10; k++){
+    if(brr[k] == arr[i]){
+        //cout << arr[i] << " "<< 
+    }
+   }
+}
+
+
+}
