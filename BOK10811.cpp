cb4b54d8bfa945dca9064e25e8c834307d441b03
Khyojae <khjae201@gmail.com>
2025-06-19 17:24:50 +0900

    250619

--- BOK10811.cpp
+++ BOK10811.cpp
@@ -7,20 +7,21 @@ int arr[100];
 
 int a,b;
 
-for(int i=0; i<=4; i++){
-    arr[i]=i+1;
+for(int i=1; i<=100; i++){
+    arr[i]=i;
 }
 
 for(int i=0; i<m; i++){ // 나머지 줄 반복해야함
 cin >> a >> b;
 
-for(i=0; i<b-a; i++){
-int temp = arr[b-i];
-arr[b-i]= arr[a+i];
-arr[a+i] = temp;
+for(int i=0; i<=(b-a)/2; i++){ // 대칭성!
+swap(arr[a+i],arr[b-i]);
 
 }
 }
 
-
+for(int i=1; i<=n; i++){
+    cout << arr[i] << ' ';
+}
+return 0;
 }
