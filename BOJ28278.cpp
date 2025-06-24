1f8ac6a3e0644e72f77764ed4d18917f600b5cdd
Khyojae <khjae201@gmail.com>
2025-06-24 10:40:08 +0900

    stack함수 연습

---
+++ BOJ28278.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+typedef int Datatype;
+typedef int element;
+class stack{
+
+
+};
+
+
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int n;
+cin >> n;
+string str;
+stack q;
+
+
+for(int i=0; i<n; i++){
+int k=0;
+cin >> str;
+if(str=="push"){
+cin >> k;
+
+}
+else if(str=="front"){
+
+cout << '\n';
+}
+else if(str=="back"){
+
+cout << '\n';
+}
+else if(str=="size"){
+
+cout << '\n';
+}
+else if(str=="empty"){
+
+cout << '\n';
+}
+else if(str=="pop"){
+
+cout << '\n';
+}
+
+}
+
+
+    
+}
