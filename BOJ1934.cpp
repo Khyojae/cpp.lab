7996f9d5c76f82843a772ec533fbbc94c63c1dd0
Khyojae <khjae201@gmail.com>
2025-06-26 18:23:06 +0900

    1934

---
+++ BOJ1934.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+int k;
+int a, b, r;
+
+int temp;
+int n;
+
+cin >> n;
+for(int i=0; i<=n; i++){
+cin >> a >>b;
+swap(a,b);
+r=a%b;
+k=a*b;
+while(r!=0){
+
+//temp=a;
+
+a=b;
+b=r;
+r=a%b;
+
+}
+cout <<  k/b << '\n';
+}
+
+}
