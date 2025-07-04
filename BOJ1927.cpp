158a80d42f9a71584f41f0509999c9ea0cde288d
Khyojae <khjae201@gmail.com>
2025-07-04 12:27:37 +0900

    최소힙 함수 연습

---
+++ BOJ1927.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<vector>
+using namespace std;
+int main(){
+int n;
+cin >> n;
+priority_queue<int, vector<int>,greater<int>>minHeap;
+for(int i=0; i<n; i++){
+int a;
+cin >> a;
+if(minHeap.empty() &&a==0){
+cout << 0 << '\n';
+continue;
+}
+else if(!minHeap.empty() && a==0){
+cout << minHeap.top() << '\n';
+minHeap.pop();
+continue;
+}
+minHeap.push(a);
+}
+
+}
