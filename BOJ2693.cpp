f0399bc032f521398649540a31f51925db8f2252
Khyojae <khjae201@gmail.com>
2025-07-04 11:11:43 +0900

    vc인덱스 접슨

---
+++ BOJ2693.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+int n;
+cin >> n;
+
+
+for(int i=0; i<n; i++){
+vector<int> vc;
+
+for(int i=0; i<=9; i++)
+{
+int a;
+cin >> a;
+vc.push_back(a);
+}
+sort(vc.begin(),vc.end());
+cout << vc[vc.size()-2] << '\n';
+}
+
+
+
+}
