3224c21a5074fd95c77df495a6b1b72736b7fc0e
Khyojae <khjae201@gmail.com>
2025-07-10 16:02:37 +0900

    10816

--- BOJ10816.cpp
+++ BOJ10816.cpp
@@ -4,28 +4,42 @@
 #include <unordered_map>
 using namespace std;
 int main(){
-ios::sync_with_stdio(false);
-cin.tie(0);
 int n,m;
 int num;
 vector <int> vc;
 vector <int> ac;
- unordered_map<int, int> ums;
-cin >> n;
-for(int i=0; i<n; i++){
+ unordered_map<int, bool> ums;
+  unordered_map<int, int> ams;
+cin >> n;    // 첫번째줄
+
+bool same= false;
+int count=1;
+for(int i=0; i<n; i++){   // 두번째줄
 cin >> num;
-ums[num]++;
+if(ums[num]==true){
+ams[num]=ams[num]+1;
+}
+else{
+ums[num]=true;
+}
 }
 
-cin >> m;
-for(int i=0; i<m; i++){
+
+cin >> m;   // 세번째줄
+for(int i=0; i<m; i++){    // 네번째줄
 cin >> num;
 ac.push_back(num);
 }
 
-//sort(ums.begin(),ums.end());
-for(int i=0; i<m; i++){
-cout << ums(ac[i])<< " ";
+
+for(int k=0; k<m; k++){
+auto it = ums.find(ac[k]);
+if(it !=ums.end()){
+cout << 1+ams[ac[k]] << " ";
+}
+else{
+cout << 0 << " ";
+}
 }
 
 
