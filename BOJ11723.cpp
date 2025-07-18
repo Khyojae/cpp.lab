82c3e05bda1926c2a3f5eccb1de8cdedf74766b9
Khyojae <khjae201@gmail.com>
2025-07-18 14:02:39 +0900

    대입 연산자로 초기화 리스트

--- BOJ11723.cpp
+++ BOJ11723.cpp
@@ -1,34 +1,72 @@
 #include <iostream>
+#include <set>
+#include <vector>
 #include <algorithm>
 using namespace std;
-
-int n,m;
-int arr[10];
-bool isused[10];
-
-void func(int k){
-if(k==m){
-for(int i=0; i<m; i++)
-    cout << arr[i] << ' ';
-cout << '\n';
-return;
-}
-
-// 1. n번 나와야해
-// 2. 순차탐색이니 출력 뭐 나올건지 check
-for(int i=1; i<=n; i++){
-if(!isused[i]){  // 되돌아올때 걸려서
-    arr[k]=i;   // 출력 뭐 나올건지
-    isused[i]=1;  // 켜고
-    func(k+1);   // 걸림
-    isused[i]=0;  // 끄기 ( 다음에 쓸려고)
-}
-}
-}
-
-
-
 int main(){
-cin >> n >> m;
-func(0);
+string str;
+int x;
+set<int>vc;
+int num=0;
+cin >> num;
+for(int i=0; i<num; i++){
+cin >> str;
+if(str=="add"){
+cin >> x;
+vc.insert(x);
 }
+
+
+if(str=="remove"){
+cin >> x;
+auto it =find(vc.begin(),vc.end(),x);
+if(it!=vc.end()){
+vc.erase(it);
+}
+}
+
+
+if(str=="check"){
+cin >> x;
+auto it =find(vc.begin(),vc.end(),x);
+
+if(it!=vc.end()){
+cout << 1 << '\n';
+}
+else{
+cout << 0 << '\n';
+}
+
+}
+
+if(str=="toggle"){
+cin >> x;
+auto it =find(vc.begin(),vc.end(),x);
+if(it!=vc.end()){
+vc.erase(it);
+}
+else{
+vc.insert(x);
+}
+}
+
+
+if(str=="all"){
+vc.clear();
+for(int i=0; i<20; i++){
+vc.insert(i+1);
+}
+}
+
+if(str=="empty"){
+vc.clear();
+}
+}
+
+}
+
+
+
+
+
+
